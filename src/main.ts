6cf67df2c430b62f44c5b1104adb41ca3f319a08
luccagoltzman <luccagoltzman@gmail.com>
2025-05-14 12:22:30 -0300

    feat: :rocket: Diretórios e arquivos principais Index e global scss

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { errorInterceptor } from './app/core/interceptors/error.interceptor';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(
+      withInterceptors([authInterceptor, errorInterceptor])
+    ),
+    provideAnimations()
+  ]
+}).catch(err => console.error(err)); 
